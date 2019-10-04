ff6b775e1b0eec347c249537d338af0da409c0b8
Evan Brown <evanbrown723@gmail.com>
2019-10-04 15:40:53 -0400

    Basic Module and CSS for Main Game Mode
    Made a basic module popup when states are clicked and fixed up some css of the svg map.

--- Team450-CSE442/Team450Project/wwwroot/js/site.js
+++ Team450-CSE442/Team450Project/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function openModule(id) {
+    let module = document.getElementById("state_module");
+
+    module.style.display = "inline-block";
+
+}
+
+function exitModule() {
+    let module = document.getElementById("state_module");
+    module.style.display = "none";
+}
+
+function submitModule() {
+    let module = document.getElementById("state_module");
+    module.style.display = "none";
+}
