2cab60bbbefe3f23538cfade97b876d801080867
Evan Brown <evanbrown723@gmail.com>
2019-10-07 12:35:42 -0400

    Revert "Merge branch 'Issue#30MainGameMode' into Development"
    This reverts commit aa7fa66ae1dd5cb81c1bb6037d973793019d306e.

--- Team450-CSE442/Team450Project/wwwroot/js/site.js
+++ Team450-CSE442/Team450Project/wwwroot/js/site.js
@@ -2,33 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-let currentID = null;
-
-function openModule(id) {
-    let module = document.getElementById("state_module");
-    let userInput = document.getElementById("input_textbox");
-    let state = document.getElementById(id);
-    currentID = id;
-
-    module.style.display = "inline-block";
-    userInput.value = "";
-    state.style.fill = "yellow";
-}
-
-function exitModule() {
-    let module = document.getElementById("state_module");
-    let state = document.getElementById(currentID);
-
-    module.style.display = "none";
-    //state.style.fill = "red";
-}
-
-function submitModule() {
-    let module = document.getElementById("state_module");
-    let userInput = document.getElementById("input_textbox");
-    let state = document.getElementById(currentID);
-
-    module.style.display = "none";
-    let userAnswer = userInput.value;
-    state.style.fill = "green";
-}
